45f36353cba2f119f62611e62fa44d1865de6e8d
Velkow <sivade.g@gmail.com>
2018-01-08 11:40:18 +0100

    fontawesome nav bar

--- admin_page/views.py
+++ admin_page/views.py
@@ -11,7 +11,6 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.template.base import logger
 
 from planning.models import Planning
 from .forms import LoginForm, UploadForm
