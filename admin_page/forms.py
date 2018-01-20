e4ae1a5e2770f773f4037da5131e2ea03883ab75
Guillaume Sivade <sivade.g@gmail.com>
2018-01-20 19:13:36 +0100

    refactor

--- admin_page/forms.py
+++ admin_page/forms.py
@@ -1,21 +1,20 @@
 import datetime
 from django import forms
-from planning.models import Planning
-from . import utils
+from supboard.enums import Grade
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label="Username", max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-input"}), label="Password", max_length=100)
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label="Username", max_length=100, required=True)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-input"}), label="Password", max_length=100, required=True)
 
 
 class ClassroomForm(forms.Form):
     today = datetime.datetime.today().strftime('%Y-%m-%d')
-    grade_1 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label="A.Sc.1", max_length=100, required=False)
-    grade_2 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label="A.Sc.2", max_length=100, required=False)
-    grade_3 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label="B.Sc", max_length=100, required=False)
-    grade_4 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label="M.Sc.1", max_length=100, required=False)
-    grade_5 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label="M.Sc.2", max_length=100, required=False)
+    grade_1 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label=Grade.ASC1.value, max_length=100, required=False)
+    grade_2 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label=Grade.ASC2.value, max_length=100, required=False)
+    grade_3 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label=Grade.BSC.value, max_length=100, required=False)
+    grade_4 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label=Grade.MSC1.value, max_length=100, required=False)
+    grade_5 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-input"}), label=Grade.MSC2.value, max_length=100, required=False)
 
 
 class UploadForm(forms.Form):
